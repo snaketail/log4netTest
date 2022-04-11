a196c909b9a5ea132ee6d254ec41845a1431253e
snaketail <51819467+snaketail@users.noreply.github.com>
2022-04-11 16:23:46 +0800

    Add files via upload
    log4net use case, colored terminal

---
+++ LogHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Log4NetTest
+{
+    public class LogHelper
+    {
+        public static log4net.ILog GetLogger([CallerFilePath]string filename="")
+        {
+            string name = Path.GetFileNameWithoutExtension(filename);
+            return log4net.LogManager.GetLogger(name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4NetTest
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();   //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            throw new Exception("abc");
+
+            log.Debug("Now Let's Debug");
+
+            log.Info("this is info message");
+
+            log.Warn("This is warning message");
+
+            log.Error("This is my error message");
+
+            Console.ReadLine();
+        }
+    }
+}
